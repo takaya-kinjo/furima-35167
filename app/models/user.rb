81a4d8780111a03eb927319c7c6ae209dd1d90f5
takaya-kinjo <beating428@gmail.com>
2021-06-22 21:27:12 +0900

    set_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,                 presence:true
+  validates :email,                    presence:true
+  validates :password,                 presence:true
+  validates :password_confimation,     presence:true
+  validates :firstname,                presence:true
+  validates :firstname_kana,           presence:true
+  validates :lastname,                 presence:true
+  validates :lastname_kana,            presence:true
+  validates :birthday,                 presence:true
 end
