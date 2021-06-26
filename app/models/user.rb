4f47de1b3236feeda7e96e00a08f9f5e561561b1
takaya-kinjo <beating428@gmail.com>
2021-06-26 19:10:37 +0900

    トップページの各項目の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname,                 presence:true
   validates :password,                 format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze }
-  validates :first_name,               presence:true
-  validates :first_name_kana,          presence:true
-  validates :last_name,                presence:true
-  validates :last_name_kana,           presence:true
+  validates :first_name,               presence:true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kana,          presence:true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name,                presence:true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana,           presence:true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,                 presence:true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe User, type: :model do
        @user.password = 'aaaaaa'
        @user.valid?
        expect(@user.errors.full_messages).to include("Password is invalid")
-       do
+       end
        it "パスワードが全角英字だけだと登録できない" do
        @user.password = 'AAA１２３'
        @user.valid?
@@ -75,6 +75,66 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
      end
+    end
+  end
+
+  describe "新規登録/本人情報確認" do
+    before do 
+      @user = FactoryBot.build(:user)
+    end
+
+    context '新規登録で本人確認が上手く行く時' do
+      it "本人確認時、本名に名字、名前を記入し、ふりがなは全てカタカナで記載しており、誕生日も記入している" do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録で本人確認が上手く行かない場合' do
+      it "名前を記載していなければ本人確認できない" do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it "名字を記載していなければ本人確認できない" do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+      it "ユーザーの名前のふりがなが、半角だと本人確認できない" do
+        @user.first_name = 'jirou'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
+      end
+      it "ユーザーの名字のふりがなが、半角だと本人確認できない" do
+        @user.last_name = 'tanaka'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
+      it "ユーザー本名の名前のふりがなが空だと登録できない" do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it "ユーザー本名の名字のふりがなが空だと登録できない" do
+        @user.last_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+      end
+      it "ユーザーの名前のふりがなが、半角だと本人確認できない" do
+        @user.first_name_kana = 'jirou'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+      it "ユーザーの名字のふりがなが、半角だと本人確認できない" do
+        @user.last_name_kana = 'tanaka'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it "生年月日が空欄だと本人確認できない" do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
     end
   end
 end
