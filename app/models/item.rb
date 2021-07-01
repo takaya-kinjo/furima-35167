b7cb00938e4c8b2acf065cc1022194325f99f734
takaya-kinjo <beating428@gmail.com>
2021-07-01 23:51:19 +0900

    ActiveHashを用いて、各モデルの作成、validationの調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :quantity
+  belongs_to :delivery_payment
+  belongs_to :delivery_prefecture
+  belongs_to :delivery_time
+
   with_options presence: true do
     validates :titele
     validates :comment
-    validates :category_id
-    validates :quantity_id
-    validates :delivery_payment_id
-    validates :delivery_prefecture_id
-    validates :delivery_time_id
     validates :preice
+
+    with_options numericality: {other_than 1} do
+      validates :category_id
+      validates :quantity_id
+      validates :delivery_payment_id
+      validates :delivery_prefecture_id
+      validates :delivery_time_id
+    end
+
   end
 end
