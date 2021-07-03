8f2334655ed2189a1b5d27ad05756f2288a0dab6
takaya-kinjo <beating428@gmail.com>
2021-07-03 13:09:49 +0900

    new, createアクションの実装、微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
 
   def new
     @items = Item.new
   end
 
+  def create
+    @items = Itme.new(item_params)
+    if @items.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+    private
+
+    def item_params
+      params.require(:item).permit(:titele, :comment, :category, :quantity, :delivery_payment, :delivery_prefecture, :delivery_time, :preice).merge(user_id: current_user.id)
+    end
 end
