8518e4475f38dcc632d0de45554d57938696c528
takaya-kinjo <beating428@gmail.com>
2021-07-24 15:44:02 +0900

    クレジットカード情報のトークン化

---
+++ app/javascript/card.js
@@ -0,0 +1,35 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_1dc04e325d0987d2e9fb170b");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_shipping_info[number]"),
+      cvc: formData.get("order_shipping_info[cvc]"),
+      exp_month: formData.get("order_shipping_info[exp_month]"),
+      exp_year: `20${formData.get("order_shipping_info[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200){
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">` ;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("order_shipping_info_number").removeAttribute("name");
+      document.getElementById("order_shipping_info_cvc").removeAttribute("name");
+      document.getElementById("order_shipping_info_exp_month").removeAttribute("name");
+      document.getElementById("order_shipping_info_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
+  });
+};
+
+window.addEventListener("load", pay);
